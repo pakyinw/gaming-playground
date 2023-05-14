6144c315614f0922a9f06814328f88c1d813f95a
pakyinw <pakyinw@hotmail.com>
2023-05-15 00:56:08 +0100

    fix: Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@
 import useDevice, { DEVICE } from './hooks/useDevice';
 import './App.css'
 import Desktop from './desktop/Desktop';
-import Mobile from './Mobile/Mobile';
+import Mobile from './mobile/Mobile';
 
 function App() {
   const { device } = useDevice()
 
-  // TODO: Check there is any performance issue whether prerendering. If
+  // TODO: Check there is any performance issue whether any prerendering before return
   const layout = {
     [DEVICE.DESKTOP]: (<Desktop/>),
     [DEVICE.PORTRAIT]: (<Mobile/>),
