fb7a55f15343acd467176928fe89cf32d4d8faf9
pakyinw <pakyinw@hotmail.com>
2023-05-15 22:58:27 +0100

    build(core): add router and remove useOrientation.

--- src/mobile/Mobile.tsx
+++ src/mobile/Mobile.tsx
@@ -1,16 +1,23 @@
-import useOrientation, { ORIENTATION } from '../hooks/useOrientation'
-import Portrait from './Portrait'
-import Landscape from './Landscape'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import LoginPage from './portrait/LoginPage'
 
+export const ROUTE_PATHS = {
+  LOGIN: '/Login',
+  LOBBY: '/Lobby',
+  SLIPS: '/Slips',
+  GAME: '/Game',
+  PROFILE: '/Profile'
+}
 
 function Mobile(){
-  const {orientation} = useOrientation()
   return (
     <div>
-      {
-        orientation === ORIENTATION.PORTRAIT ?
-          <Portrait/> :<Landscape/>
-      }
+      <BrowserRouter>
+        <Routes>
+          <Route path={ROUTE_PATHS.LOGIN} element={<LoginPage/>} />
+          <Route path="*" element={<Navigate to={ROUTE_PATHS.LOGIN} replace />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
