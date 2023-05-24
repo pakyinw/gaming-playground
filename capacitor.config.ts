b0a5f90d229e9ae7b5e5a256d77a3312afd234e2
pakyinw <pakyinw@hotmail.com>
2023-05-25 00:15:58 +0100

    build(core): update the capacitor setting

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.cashsite.app',
   appName: 'cashsite',
-  webDir: 'build',
+  webDir: 'dist',
   server: {
     androidScheme: 'https'
   }
